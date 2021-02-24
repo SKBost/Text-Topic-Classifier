796dc41641f5f6eac2eb308911774fd9cb195e8f
SKBost <ebost@ucsd.edu>
2021-02-24 01:33:38 -0800

    logistic regression and graident descent algorithms written, but training error is wrong

--- LinearClassifier.java
+++ LinearClassifier.java
@@ -38,62 +38,6 @@ public class LinearClassifier {
         binaryTrPts = binaryPts;
     }
 
-    /*
-    public void runMethodTests() {
-        //testMSAV(); // good
-        //testGDP(); // good
-        //testSV(); // good
-
-    }
-
-    private void testMSAV() {
-        double scalar = 3.0;
-        double[] vector = new double[3];
-        vector[0] = 1;
-        vector[1] = 2;
-        vector[2] = 3;
-        double[] newVec = multiplyScalarAndVector(scalar, vector);
-        printDoubleArray(newVec);
-
-        // expect: [3, 6, 9]
-    }
-
-    private void testGDP() {
-        double[] vector1 = new double[3];
-        vector1[0] = 1;
-        vector1[1] = 2;
-        vector1[2] = 3;
-
-        double[] vector2 = new double[3];
-        vector2[0] = 4;
-        vector2[1] = 5;
-        vector2[2] = 6;
-
-        double dp = getDotProduct(vector1, vector2);
-        print("" + dp);
-
-        // expect: 32
-    }
-
-    private void testSV() {
-        double[] vector1 = new double[3];
-        vector1[0] = 1;
-        vector1[1] = 2;
-        vector1[2] = 3;
-
-        double[] vector2 = new double[3];
-        vector2[0] = 4;
-        vector2[1] = 5;
-        vector2[2] = 6;
-
-        double[] newVec = sumVectors(vector1, vector2);
-        printDoubleArray(newVec);
-
-        // expect: [5, 7, 9]
-
-    }
-    */
-
     public void useClasses1And2() {
         trPts = binaryTrPts;
     }
@@ -118,35 +62,6 @@ public class LinearClassifier {
         printDoubleArray(newVec);
         */
         return newVec;
-    }
-
-    //MEGA TODO!!!!! VITODO FIX THIS SO IT ALL WORKS ON OTHER VALUES INSTEAD OF EXPECTING -1 AND 1
-    // runs a single pass of perceptron given a starting value
-    public double[] teachPerceptron(double[] w0) { // note: we start at w0 instead of w1 for 0 indexing
-        //print("inside teachPerceptron");
-        double[][] w = new double[trPts.size() + 1][numFeatures]; // todo: do we actually use numPoints + 1?
-        w[0] = w0;
-        //print("each w is size " + w[0].length);
-        //print("there are " + w.length + " ws");
-
-        for(int t = 0; t < trPts.size(); t++) {
-            double yt = trPts.get(t).getLabel();
-            double[] xt  = trPts.get(t).getFeatures();
-            double classVal = yt * getDotProduct(w[t], xt); // classification value
-
-            if(classVal <= 0) {
-                //print("perceptron got it wrong on training point t = " + t + ", where classVal is " 
-                    //+ classVal + "; updating w[t+1]");
-                w[t+1] = sumVectors(w[t], multiplyScalarAndVector(yt, xt));
-            } else {
-                //print("perceptron got it right on training point t = " + t + ", where classVal is " 
-                    //+ classVal + "; not updating w[t+1]");
-                w[t+1] = w[t];
-            }
-        }
-
-        //print("trPts.size() is " + trPts.size());
-        return w[trPts.size()]; // final form of w after running on last data point
     }
 
     // tested
@@ -184,14 +99,35 @@ public class LinearClassifier {
         return multDimSum;
     }
 
-    /*
-    // this method might be trivial
-    private double[] getZeroVector() {
-        double[] zeroVec = new double[dimNum];
-        return zeroVec;
-    }
-    */
+  // perceptron
 
+    // runs a single pass of perceptron given a starting value
+    public double[] teachPerceptron(double[] w0) { // note: we start at w0 instead of w1 for 0 indexing
+    //print("inside teachPerceptron");
+    double[][] w = new double[trPts.size() + 1][numFeatures]; // todo: do we actually use numPoints + 1?
+    w[0] = w0;
+    //print("each w is size " + w[0].length);
+    //print("there are " + w.length + " ws");
+
+    for(int t = 0; t < trPts.size(); t++) {
+        double yt = trPts.get(t).getLabel();
+        double[] xt  = trPts.get(t).getFeatures();
+        double classVal = yt * getDotProduct(w[t], xt); // classification value
+
+        if(classVal <= 0) {
+            //print("perceptron got it wrong on training point t = " + t + ", where classVal is " 
+                //+ classVal + "; updating w[t+1]");
+            w[t+1] = sumVectors(w[t], multiplyScalarAndVector(yt, xt));
+        } else {
+            //print("perceptron got it right on training point t = " + t + ", where classVal is " 
+                //+ classVal + "; not updating w[t+1]");
+            w[t+1] = w[t];
+        }
+    }
+
+    //print("trPts.size() is " + trPts.size());
+    return w[trPts.size()]; // final form of w after running on last data point
+}
 
     public void runPerceptronPasses(int numPasses) {
         print("inside runPerceptronPasses");
@@ -199,9 +135,9 @@ public class LinearClassifier {
         for(int i = 0; i < numPasses; i++) {
             double[] wT = teachPerceptron(w0); // the final value/output
             double error = getPerceptronTrainingError(wT);
-            print("after run " + i + " of teaching perceptron, we get the following training error:" + error);
+            print("after run " + i + " of teaching perceptron, we get the following training error: " + error);
             error = getPerceptronError(wT, testPts);
-            print("after run " + i + " of teaching perceptron, we get the following test error:" + error);
+            print("after run " + i + " of teaching perceptron, we get the following test error: " + error);
             //printDoubleArray(w0);
             w0 = wT; // the final value/output of each pass is the starter for the next pass
         }
@@ -252,6 +188,149 @@ public class LinearClassifier {
     public double getPerceptronTrainingError(double[] classifier) {
         return getPerceptronError(classifier, trPts);
     }
+
+
+
+    // logistic regression/gradient descent
+
+    /*public void runIterationsOfGradientDescent(int numIterations) {
+        print("inside runGradientDescent");
+        double[] w0 = new double[numFeatures]; // starter values of 0 for logistic regression to build off
+        for(int i = 0; i < numIterations; i++) {
+            double[] wT = runGradientDescent(w0); // the final value/output
+            double error = getLogisticRegressionTrainingError(wT);
+            print("after run " + i + " of gradient descent, we get the following training error: " + error);
+            error = getPerceptronError(wT, testPts);
+            print("after run " + i + " of gradient descent, we get the following test error: " + error);
+            //printDoubleArray(w0);
+            w0 = wT; // the final value/output of each pass is the starter for the next pass
+    }*/
+
+    public double[] runGradientDescent(int numIterations) {
+            //print("inside runGradientDescent");
+        double rate = 0.001; // learning rate/step size
+        double stopThreshold = 0.001; // todo check if this is good for this program
+        double[][] w = new double[trPts.size() + 1][numFeatures]; // todo: do we actually use numPoints + 1?
+        w[0] = new double[numFeatures];
+        //print("each w is size " + w[0].length);
+        //print("there are " + w.length + " ws");
+
+        for(int t = 0; t < numIterations; t++) { // VITDO: CHECK IF THIS IS EVEN SUPPOSED TO RUN THIS MANY TIMES?
+
+            double[] deltaLW = getDeltaLW(w[t]);
+            w[t+1] = subtractVectors(w[t], multiplyScalarAndVector(rate, deltaLW));
+            
+            if(norm(deltaLW) <= stopThreshold) {
+                return w[t + 1];
+            } 
+        }
+
+        //print("trPts.size() is " + trPts.size());
+        return w[trPts.size()]; // final form of w after running on last data point
+    }
+
+    // function that gives most of the update for w[t+1]
+    private double[] getDeltaLW(double[] w) { 
+        // note: may be worth changing variable names to be more descriptive, 
+        // but since I'm trying to make them match an algorithm, maybe not
+
+        double[] vecSum = new double[w.length];
+        
+        for(int i = 0; i < trPts.size(); i++) {
+            DataPoint dp = trPts.get(i);
+            double[] xi = dp.getFeatures();
+            double yi = dp.getLabel();
+
+            
+            double[] vecNumer = multiplyScalarAndVector(-yi, xi); // vector numerator
+            double[] vecI = multiplyScalarAndVector(sigmoid(yi, xi, w), vecNumer);
+            vecSum = sumVectors(vecSum, vecI);
+        }
+
+        return vecSum;
+    }
+
+    // this sigmoid functuon gives P(y | x)
+    private double sigmoid(double y, double[] x, double[] w) {
+            double exponent = y * getDotProduct(w, x);
+            double sigScal = 1/(1 + Math.exp(exponent)); // sigmoid scalar
+            return sigScal;
+    }
+
+    // w is the classifier (todo: change name)
+    // perceptron gives correct classification
+    private boolean logisticRegressionGivesCorrectClassification(double[] w, DataPoint testDP) {
+        double yt = testDP.getLabel();
+        double[] xt  = testDP.getFeatures();
+        // classification value: return true if chances are greater than half that the label is 
+        // the correct label (yt) given xt according to the predictor w
+        double classVal = sigmoid(yt, xt, w); 
+        return classVal > 0.5;
+    }
+
+    public double getLogisticRegressionTrainingError(double[] classifier) {
+        return getLogisticRegressionError(classifier, trPts);
+    }
+
+    private double getLogisticRegressionError(double[] classifier, ArrayList<DataPoint> testData) {
+        int timesCorrect = 0;
+        int timesIncorrect = 0;
+
+        // for each line of data with a feature vector and label
+        for(int i = 0; i < testData.size(); i++) {
+
+            DataPoint testDP = testData.get(i);
+            double label = testDP.getLabel();
+
+            // classify based on training data with our perceptron
+            if(logisticRegressionGivesCorrectClassification(classifier, testDP)) { // if classification is correct
+                timesCorrect++; // increment how many times we got it right
+            } else {
+                timesIncorrect++;
+                //print("for line " + i + " of the test data, we " + 
+                 //     " incorrectly label it as " + classification + 
+                //     " instead of the correct label, " + label);
+                    
+            }
+            
+        }
+
+        double error = ((double) timesIncorrect) / testData.size();
+        //print("error  = " + error);
+
+        //print("--------------------------------------------");
+
+        return error;
+    }
+
+    // TODO
+    private double[] subtractVectors(double[] first, double[] second) {
+        //print("inside subtractVectors");
+
+        double[] newVec = new double[first.length];
+        
+        if(first.length != second.length) {
+            print("ERROR in subtractVectors: first.length != second.length; returning placeholder zero vector");
+        } else {
+            for(int i = 0; i < first.length; i++) {
+                newVec[i] = first[i] - second[i];
+            }
+        }
+
+        return newVec;
+    }
+
+    // TODO
+    private double norm(double[] vector) {
+        double sum = 0;
+
+        for(int i = 0; i < vector.length; i++) {
+            sum += vector[i] * vector[i];
+        }
+
+        return Math.sqrt(sum);
+    }
+
 
     public static void printDoubleArray(double[] array) {
         System.out.print("[");
@@ -309,3 +388,67 @@ public class LinearClassifier {
         System.out.println(text);
     }
 }
+
+ /*
+    public void runMethodTests() {
+        //testMSAV(); // good
+        //testGDP(); // good
+        //testSV(); // good
+
+    }
+
+    private void testMSAV() {
+        double scalar = 3.0;
+        double[] vector = new double[3];
+        vector[0] = 1;
+        vector[1] = 2;
+        vector[2] = 3;
+        double[] newVec = multiplyScalarAndVector(scalar, vector);
+        printDoubleArray(newVec);
+
+        // expect: [3, 6, 9]
+    }
+
+    private void testGDP() {
+        double[] vector1 = new double[3];
+        vector1[0] = 1;
+        vector1[1] = 2;
+        vector1[2] = 3;
+
+        double[] vector2 = new double[3];
+        vector2[0] = 4;
+        vector2[1] = 5;
+        vector2[2] = 6;
+
+        double dp = getDotProduct(vector1, vector2);
+        print("" + dp);
+
+        // expect: 32
+    }
+
+    private void testSV() {
+        double[] vector1 = new double[3];
+        vector1[0] = 1;
+        vector1[1] = 2;
+        vector1[2] = 3;
+
+        double[] vector2 = new double[3];
+        vector2[0] = 4;
+        vector2[1] = 5;
+        vector2[2] = 6;
+
+        double[] newVec = sumVectors(vector1, vector2);
+        printDoubleArray(newVec);
+
+        // expect: [5, 7, 9]
+
+    }
+    */
+
+      /*
+    // this method might be trivial
+    private double[] getZeroVector() {
+        double[] zeroVec = new double[dimNum];
+        return zeroVec;
+    }
+    */
--- LinearClassifierRunner.java
+++ LinearClassifierRunner.java
@@ -10,6 +10,7 @@ public class LinearClassifierRunner {
     public static void main(String[] args) {
         int dimNum = 819;
         int numPasses = 4;
+        int numIterations = 2;
         String trainFile = "pa3train.txt";
         String testFile = "pa3test.txt";
 
@@ -20,9 +21,18 @@ public class LinearClassifierRunner {
         lc.useClasses1And2();
         print("Part 1:");
         lc.runPerceptronPasses(numPasses);
-        //lc.runMethodTests();
+
 
         // part 2
+
+        print("Part 2:");
+        double[] wT = lc.runGradientDescent(numIterations);
+        //lc.runMethodTests();
+
+        double error = lc.getLogisticRegressionTrainingError(wT);
+        print("after " + numIterations + " runs of gradient descent, we get the following training error: " + error);
+
+        
 
 
 
